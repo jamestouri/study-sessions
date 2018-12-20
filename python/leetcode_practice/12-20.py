34d4577f4f11e2fe53c386042892cb5c616ea6fe
jamestouri <j_touri@yahoo.com>
2018-12-20 12:41:00 -0800

    stuck on the array

--- python/leetcode_practice/12-20.py
+++ python/leetcode_practice/12-20.py
@@ -6,8 +6,7 @@ def shiftingLetters(S, shifts):
             break
         shifted = ord(S[i]) + (total % 26)
         if shifted > ord('z'):
-            while shifted > ord('z'):
-                shifted = (shifted - ord('z')) + (ord('a') - 1)
+            shifted = (shifted - ord('z')) + (ord('a') - 1)
         res += chr(shifted)
         total -= shifts[i]
     return res
@@ -15,4 +14,22 @@ def shiftingLetters(S, shifts):
 a = "bad"
 b = [10,20,30]
 
-print(shiftingLetters(a, b))
+
+def setZeroes(matrix):
+    i_set = set()
+    j_set = set()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 0:
+                if i not in i_set and j not in j_set:
+                    i_set.add(i)
+                    j_set.add(j)
+                    x = i
+                    y = j
+                    while x < len(matrix) - 1:
+                        matrix[x][j] = 0
+                        x += 1
+                    while y < len(matrix[i]) - 1:
+                        matrix[i][y] = 0
+                        y += 1
+                    
